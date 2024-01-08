3cccecccbdc17a538cb540445d52b6e09aaae05e
Natanael Vieira <natanaelsales@live.com>
2024-01-08 07:32:11 -0300

    Create ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+/*console.log('Tudo bem?!')
+console.log('Tudo bem?!')
+console.log('Tudo bem?!')
+console.log('Tudo bem?!')
+console.log('Tudo bem?!')
+console.log('Tudo bem?!')*/
+
+var c = 1
+// while (c <= 6) {
+//     console.log(`Passo ${c}`)
+//     c++
+// }
+
+do {
+    console.log(`Contando de 1 em 1 = ${c}`)
+    c++
+} while (c <= 10) 
