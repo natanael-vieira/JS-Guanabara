4a0b9ba223bdc59220a1e25b1b443cea7d9956db
Natanael Vieira <natanaelsales@live.com>
2024-01-08 05:22:57 -0300

    Update script.js

--- aula12/Exercícios/ex016/script.js
+++ aula12/Exercícios/ex016/script.js
@@ -1,1 +1,12 @@
-alert('Olá')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fAno = window.document.getElementById('txtano')
+    var res = window.document.querySelector('div#res')
+
+    if (fAno.value.length == 0 || Number(fAno.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        window.alert('Agora sim ^^')
+    }
+}
