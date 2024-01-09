2b1d19778e3bfaddc44e123a1dec9fe145bb2d2c
Natanael Vieira <natanaelsales@live.com>
2024-01-09 06:53:35 -0300

    Create ambiente.js

---
+++ aula14/ambiente.js
@@ -0,0 +1,17 @@
+// var c = 1
+
+// while(c <= 5) {
+//     console.log(c)
+//     c++
+// }
+
+// var d = 1
+
+// do {
+//     console.log(d)
+//     d++
+// } while (d <= 13)
+
+for (var a = 1; a <= 9; a++) {
+    console.log(a)
+}
