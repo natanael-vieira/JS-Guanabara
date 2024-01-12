6934cd1196728103062a2fd011570e6d643850dc
Natanael Vieira <natanaelsales@live.com>
2024-01-12 04:56:23 -0300

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -5,3 +5,7 @@ num.sort()
 console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+for (var pos = 0; pos < num.length ; pos++) {
+    console.log(`A posição ${pos} tem o valor: ${num[pos]}`)
+}
