d9489fe91311633abc2fa8a751f24efa9a6205da
Natanael Vieira <natanaelsales@live.com>
2024-01-12 05:11:41 -0300

    Update ambiente.js

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -6,3 +6,10 @@ console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
 
+let pos = num.indexOf(5)
+
+if (pos == -1) {
+    console.log('O valor não existe no array')
+} else {
+    console.log(`O valor está na posição: ${pos}`)
+}
